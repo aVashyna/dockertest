0f69b9f5844924c02d9ce5cb88417c9cb46914a1
avashynaleverx <aliaksandr.vashyna@leverx.com>
2021-02-01 15:47:00 +0300

    Changed message

--- docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -4,7 +4,7 @@ public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
 		for(int i = 0 ; i < 100; i++){
-			System.out.println("Hello World from Git Ping " + i );
+			System.out.println("Hello Docker from Git Ping " + i );
 			Thread.sleep(1000);
 		}
 	}
